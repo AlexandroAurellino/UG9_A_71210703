bcbda5815a3accab250bb8396a99c698894e9340
AlexandroAurellino <93307651+AlexandroAurellino@users.noreply.github.com>
2021-11-11 21:55:21 +0700

    Soal 3 revisi

---
+++ 3_A_71210703.py
@@ -0,0 +1,11 @@
+#Data
+Item1 = input("Masukkan daftar belanja 1: ")
+Item2 = input("Masukkan daftar belanja 2: ")
+Itemtambah1 = input("Tambahkan data ke daftar belanja 1: ")
+Itemtambah2 = input("Tambahkan data ke daftar belanja 2: ")
+
+
+#Proses
+print("Daftar belanja 1 adalah", Item1 +","+Itemtambah1)
+print("Daftar belanja 2 adalah", Item2 +","+Itemtambah2)
+
